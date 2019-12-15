ca84876e4f5583b77c74221f20b34d0f56ca06bf
Lysenko Igor <programmer.ubuntu@gmail.com>
2019-12-15 14:13:41 +0200

    Fixed tests

--- tests/unit/test_bot_utils.py
+++ tests/unit/test_bot_utils.py
@@ -1,6 +1,7 @@
 import pytest
 
-from telegram_error_notifications.utils import get_parameters
+from telegram_error_notifications.utils import get_parameters, \
+    get_exception_type
 
 
 @pytest.fixture
@@ -30,6 +31,21 @@ def get_request_without_chat():
     return request
 
 
+@pytest.fixture
+def telegram_error_message():
+    return '''Test Project for Bot. Internal Server Error: /
+
+Request Method: GET
+Request URL: http://localhost:8000/
+Django Version: 2.2
+Python Version: 3.8.0
+Server time: Sun, 15 Dec 2019 11:23:42  0000
+
+Exception Type: ValueError at /
+
+Details URL: http://localhost:8000/bot/errors/805a34b6-e8cf-48a7-b025-aa378e445779/'''
+
+
 def test_getting_of_proper_parameters(get_proper_request):
     assert get_parameters(get_proper_request) == ('some_command', 540784105)
 
@@ -45,3 +61,6 @@ def test_getting_of_parameters_without_chat(get_request_without_chat):
 def test_getting_of_parameters_without_request():
     assert get_parameters(b'') is None
 
+
+def test_getting_of_exception_type(telegram_error_message):
+    assert get_exception_type(telegram_error_message) == 'ValueError'
